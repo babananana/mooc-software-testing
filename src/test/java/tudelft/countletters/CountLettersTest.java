b208983a782e3245ef86187ffea4243c8d05db18
babananana <babananana@users.noreply.github.com>
2018-11-25 16:17:13 +0100

    Week3 CountLetter exercise

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -2,19 +2,30 @@ package tudelft.countletters;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CountLettersTest {
 
-    @Test
-    public void multipleMatchingWords() {
-        int words = new CountLetters().count("cats|dogs");
-        Assertions.assertEquals(2, words);
+    @ParameterizedTest(name = "input={0}, expectedResult={1}")
+    @CsvSource({
+            "cats|dogs,2",
+            "foxes|dogs,2",
+            "cats|foxes,2"
+    })
+    public void multipleMatchingWords(String inputString, int expectedResult) {
+        int words = new CountLetters().count(inputString);
+        Assertions.assertEquals(expectedResult, words);
     }
 
-    @Test
-    public void lastWordDoesNotMatch() {
-        int words = new CountLetters().count("cats|dog");
-        Assertions.assertEquals(1, words);
+    @ParameterizedTest(name = "input={0}, expectedResult={1}")
+    @CsvSource({
+            "cats|dog,1",
+            "cats|fox,1"
+    })
+    public void lastWordDoesNotMatch(String inputString, int expectedResult) {
+        int words = new CountLetters().count(inputString);
+        Assertions.assertEquals(expectedResult, words);
     }
 
 }
